b2845fce685b0db7bc33e82187a427960c24526f
Denis <91386420+GaydakDenis@users.noreply.github.com>
2026-06-19 11:09:19 +0300

    Add files via upload

---
+++ practice2/ex3.rs
@@ -0,0 +1,10 @@
+fn main() {
+    let x: i32 = 10;
+    let y: i32 = 5; // перенесли сюди
+
+    {
+        println!("Inner scope value of x is {} and value of y is {}", x, y);
+    }
+
+    println!("Outer scope value of x is {} and value of y is {}", x, y); 
+}
