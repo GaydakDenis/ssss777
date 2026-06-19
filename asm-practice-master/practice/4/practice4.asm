85dfddcbd9c3e8defd2a4f7b875e61aa11caf529
Denis <91386420+GaydakDenis@users.noreply.github.com>
2026-06-19 13:21:51 +0300

    Update practice4.asm

--- asm-practice-master/practice/4/practice4.asm
+++ asm-practice-master/practice/4/practice4.asm
@@ -1,36 +1,83 @@
-; practice3.asm
-; I/O: int 80h
-; blocks: I/O, parse, math/logic, loops, memory
-
 BITS 32
 GLOBAL _start
 
 SECTION .data
-prompt db "practice3: see README.md", 10
-prompt_len equ $-prompt
+    prompt db "practice3: see README.md", 10
+    prompt_len equ $-prompt
+    newline db 10
 
 SECTION .bss
-buf resb 256
+    buf resb 256
+    out_buf resb 16
 
 SECTION .text
 _start:
-    ; I/O: write prompt
-    mov eax, 4          ; sys_write
-    mov ebx, 1          ; stdout
+    mov eax, 4          
+    mov ebx, 1          
     mov ecx, prompt
     mov edx, prompt_len
     int 0x80
 
-    ; I/O: read line (optional in skeleton)
-    mov eax, 3          ; sys_read
-    mov ebx, 0          ; stdin
+    mov eax, 3          
+    mov ebx, 0          
     mov ecx, buf
     mov edx, 255
     int 0x80
 
-    ; logic: TODO implement task logic according to README.md
+    mov esi, buf
+    xor eax, eax        
+    mov ebx, 10         
 
-    ; exit
-    mov eax, 1          ; sys_exit
+.loop_parse:
+    movzx ecx, byte [esi]
+    
+    cmp cl, 10          
+    je .parse_done
+    cmp cl, 0           
+    je .parse_done
+    cmp cl, '0'         
+    jb .parse_done
+    cmp cl, '9'         
+    ja .parse_done
+
+    sub cl, '0'         
+    mul ebx             
+    add eax, ecx        
+    
+    inc esi
+    jmp .loop_parse
+
+.parse_done:
+    and eax, 0xFFFF     
+
+    mov ecx, out_buf        
+    add ecx, 15         
+    mov ebx, 10         
+    mov edi, 0          
+
+.loop_convert:
+    xor edx, edx        
+    div ebx             
+
+    add dl, '0'         
+    dec ecx             
+    mov [ecx], dl       
+    inc edi             
+
+    test eax, eax       
+    jnz .loop_convert   
+
+    mov eax, 4          
+    mov ebx, 1          
+    mov edx, edi        
+    int 0x80
+
+    mov eax, 4          
+    mov ebx, 1          
+    mov ecx, newline
+    mov edx, 1
+    int 0x80
+
+    mov eax, 1          
     xor ebx, ebx
     int 0x80
