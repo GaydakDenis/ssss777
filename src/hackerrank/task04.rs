497a0040ad188878af5d6b59f0cdc20b52d3d02a
Denis <91386420+GaydakDenis@users.noreply.github.com>
2026-06-19 11:11:42 +0300

    Update task04.rs

--- src/hackerrank/task04.rs
+++ src/hackerrank/task04.rs
@@ -1,1 +1,21 @@
-// https://www.hackerrank.com/challenges/grading/problem
+pub fn grading_students(grades: Vec<u32>) -> Vec<u32> {
+    grades.into_iter().map(|g| {
+        if g >= 38 && g % 5 > 2 {
+            g + (5 - g % 5)
+        } else {
+            g
+        }
+    }).collect()
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_example() {
+        let input = vec![73, 67, 38, 33];
+        let expected = vec![75, 67, 40, 33];
+        assert_eq!(grading_students(input), expected);
+    }
+}
